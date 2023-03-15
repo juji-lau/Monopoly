fc537b00dace3be685e665bfbeb2889296c6133e
Alexandra Martschenko <aom32@cornell.edu>
2023-03-15 13:36:20 -0400

    Created test suite

---
+++ test/main.ml
@@ -0,0 +1,9 @@
+open OUnit2
+open Board
+open Player
+
+let current_location_test (name : string) (input : string) (expected_output : int) : test =
+  name >:: fun _ ->
+    assert_equal expected_output (input |> Player.new_player |> Player.current_location)
+
+
