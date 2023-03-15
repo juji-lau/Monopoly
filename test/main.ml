4e7ce799b38101ae963fd480f222b84a7be99cb8
Alexandra Martschenko <aom32@cornell.edu>
2023-03-15 13:52:55 -0400

    Added a Makefile

--- test/main.ml
+++ test/main.ml
@@ -1,3 +1,14 @@
-
+open OUnit2
+open Board
+open Player
 let current_location_test (name : string) (input : string) (expected_output:int) : test = 
   name >:: fun _ -> assert_equal expected_output (input |> Player.new_player |> Player.current_location)
+
+let player_tests = [
+  current_location_test "start" "Alexandra" 0;
+]
+  let suite =
+  "test suite for Monopoly"
+  >::: List.flatten [ player_tests ]
+
+let _ = run_test_tt_main suite
