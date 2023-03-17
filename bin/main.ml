0d8675492c8c0de87845efc262cef0bd102fd8cf
Alexandra Martschenko <aom32@cornell.edu>
2023-03-17 17:30:57 -0400

    Implemented basic terminal functionality
    - Added command.ml and command.mli
    - Added main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -6,15 +6,32 @@ open Player
 let play_game f = raise (Failure "Unimplemented: Main.play_game")
 let data_dir_prefix = "data" ^ Filename.dir_sep
 
+
+
+type flow =
+|Play
+|End
+
+(** [player state adv flow] parses the commands of the user into an action of the player. *)
+let rec player flow play=
+  print_endline("Please enter a command.");
+  if flow = Play then
+    begin
+    match read_line() with 
+    |str -> let command = Command.parse str in
+    match command with
+    |Command.Quit -> print_endline("the game has ended."); player End play
+    |Command.Roll -> print_endline("dice rolled");  player Play (Player.move_to play)
+    end
+  else print_endline("the game has ended"); player End play
+
+
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
   ANSITerminal.print_string [ ANSITerminal.red ] "\n\nWelcome to MONOPOLY! \n";
   print_endline "Please enter your name: \n";
   print_string "> ";
   match read_line () with
-  | x -> new_player x
-(* match read_line () with | exception End_of_file -> () | file_name ->
-   play_game (data_dir_prefix ^ file_name ^ ".json") *)
-
+  | x -> player Play (Player.new_player x); ()
 (* Execute the game engine. *)
 let () = main ()
---
+++ src/command.mli
@@ -0,0 +1,30 @@
+(** Parsing of player commands. *)
+
+(**********************************************************************
+ * DO NOT CHANGE THIS FILE
+ * It is part of the interface the course staff will use to test your
+ * submission.
+ **********************************************************************)
+
+(** The type [command] represents a player command that is decomposed into a
+    verb and possibly an object phrase. Invariant: the [object_phrase] carried
+    by [Go] must not be empty. *)
+type command =
+  | Roll
+  | Quit
+
+exception Empty
+(** Raised when an empty command is parsed. *)
+
+exception Malformed
+(** Raised when a malformed command is parsed. *)
+
+val parse : string -> command
+(** [parse str] parses a player's input into a [command].
+    Requires: [str] contains only alphanumeric (A-Z, a-z, 0-9) and space
+    characters (only ASCII character code 32; not tabs or newlines, etc.).
+
+    Raises: [Empty] if [str] is the empty string or contains only spaces.
+
+    Raises: [Malformed] if the command is malformed. A command is malformed if
+    the verb is neither "quit" nor "roll". *)
