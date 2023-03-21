21e770275078e9d67e329a328ff1de1e298b9a53
Sophie Zhao <sz266@cornell.edu>
2023-03-21 14:08:07 -0400

    Reformatted player.ml

--- bin/main.ml
+++ bin/main.ml
@@ -4,28 +4,37 @@ open Player
 (** [play_game f] starts the adventure in file [f]. *)
 
 type flow =
-|Play
-|End
+  | Play
+  | End
 
-(** [player state adv flow] parses the commands of the user into an action of the player. *)
-let rec player flow play=
-try
-  print_endline("Please enter a command.");
-  print_string "> ";
-  if flow = Play then
-    begin
-    match read_line() with 
-    |str -> let command = Command.parse str in
-    match command with
-    |Command.Quit -> print_endline("The game has ended."); player End play
-    |Command.Roll -> let nplay = Player.move play in 
-    print_endline("Dice rolled. New position is: " ^ (string_of_int (Player.current_location nplay))); 
-     player Play nplay
-    end
-  else print_endline("the game has ended") with
-  |Command.Empty -> print_endline("Please enter a nonempty command."); player Play play
-  |Command.Malformed -> print_endline("Please enter a valid command."); player Play play
-
+(** [player state adv flow] parses the commands of the user into an action of
+    the player. *)
+let rec player flow play =
+  try
+    print_endline "Please enter a command (roll or quit).";
+    print_string "> ";
+    if flow = Play then
+      match read_line () with
+      | str -> (
+          let command = Command.parse str in
+          match command with
+          | Command.Quit ->
+              print_endline "The game has ended.";
+              player End play
+          | Command.Roll ->
+              let nplay = Player.move play in
+              print_endline
+                ("Dice rolled. New position is: "
+                ^ string_of_int (Player.current_location nplay));
+              player Play nplay)
+    else print_endline "The game has ended, thanks for playing!"
+  with
+  | Command.Empty ->
+      print_endline "Please enter a nonempty command.";
+      player Play play
+  | Command.Malformed ->
+      print_endline "Please enter a valid command.";
+      player Play play
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
@@ -33,6 +42,9 @@ let main () =
   print_endline "Please enter your name: \n";
   print_string "> ";
   match read_line () with
-  | x -> player Play (Player.new_player x); ()
+  | x ->
+      player Play (Player.new_player x);
+      ()
+
 (* Execute the game engine. *)
 let () = main ()
