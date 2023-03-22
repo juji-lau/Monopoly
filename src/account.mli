dd6d6fabf837f3a698f004d6f38fb93b1a45e07b
Alexandra Martschenko <aom32@cornell.edu>
2023-03-22 16:54:34 -0400

    Implemented account.ml and account.mli
    - Added paying and recieving functions

---
+++ src/account.mli
@@ -0,0 +1,17 @@
+(** Representation of player bank account. 
+
+    This module represents the data stored in the account of each player.
+    It handles buying properties and houses, paying taxes and recieving money. *)
+
+type t 
+(** The abstract value of a players bank account. *)
+
+val init : t
+(** [init] initializes a new account with 1500 dollars. *)
+
+val pay : int -> t -> t
+(** [pay i a] removes [i] dollars from account [a]. If [a] has less than [i] dollars in 
+    their account then raises Broke. *)
+
+val recieve : int ->t -> t 
+(** [ recieve i a ] adds [i] dollars to account [a]. *)
--- src/board.ml
+++ src/board.ml
@@ -1,16 +1,4 @@
-(**
-type t = {
-  current : int
-  }
 
-
-let init =  {current = 0}
-let position b = b.current
-
-let move_to b i = 
-  {current = b.current +i}
-  
-*)
 
 type t = {
   board : Position.square list;
