0640603034396531261a85094479474b4e3a93f1
Juji Lau <jal499@cornell.edu>
2023-04-16 18:53:09 -0400

    Very Basic board outline, no lines
    - literally a square in the middle of the page
    - will add gridlines next

--- src/gui.ml
+++ src/gui.ml
@@ -1,12 +1,49 @@
 open Raylib
 
+(*window size*)
+
+(*window width and height; width > height*)
 let width = 1000
 let height = 500
 let text_color = Color.black
 
+(*board*)
+let board_background_color = Color.darkgreen
+let board_line_color = Color.black
+
+(* height of the board; must be less than 1 *)
+let b_height = height * 3/4
+
+(* must be less than 1 *)
+let prop_from_top = 0.8
+(*let board_length = width * 3 / 4*)
+
+(* (0,0) is the top left of the window; (0, height) is the bottom left of the
+   window *)
+let board_tl =
+  ( (width - b_height) / 2,
+    int_of_float (float_of_int (height - b_height) *. prop_from_top) )
+
+let board_tr =
+  ( (width / 2) + (b_height / 2),
+    int_of_float (float_of_int (height - b_height) *. prop_from_top) )
+
+let board_bl =
+  ( (width - b_height) / 2,
+    int_of_float (float_of_int (height - b_height) *. prop_from_top) + b_height
+  )
+
+let board_br =
+  ( (width / 2) + (b_height / 2),
+    int_of_float (float_of_int (height - b_height) *. prop_from_top) + b_height
+  )
+
 let center_text string =
   let font_size = width / String.length string * 3 / 4 in
-  draw_text string ((width - ((String.length string)* font_size))/2) ((height + font_size)/2) font_size text_color
+  draw_text string
+    ((width - (String.length string * font_size)) / 2)
+    ((height + font_size) / 2)
+    font_size text_color
 
 let setup () =
   Raylib.init_window width height "raylib [core] example - basic window";
@@ -16,16 +53,39 @@ let setup () =
 let rec check_start () =
   begin_drawing ();
   clear_background Color.green;
-  (*draw_text "Let's Play!" (width / 4) (height / 2) (height / 15) Color.black;*)
+  (*draw_text "Let's Play!" (width / 4) (height / 2) (height / 15)
+    Color.black;*)
   center_text "Let's Play!";
   draw_text "Press B for back"
     (width - (width / 7))
     (height / 25) (height / 30) Color.red;
   end_drawing ();
-  (* if B is pressed, keep drawing this page, otherwise, leave function to the first page *)
+  (* if B is pressed, keep drawing this page, otherwise, leave function to the
+     first page *)
   if is_key_down Key.B == false then check_start ()
 
-let rec draw_board() = begin_drawing(); draw_grid
+(*tentatively draw the base game board*)
+let rec draw_board () =
+  begin_drawing ();
+  clear_background Color.white;
+  draw_rectangle (fst board_tl)
+    (snd board_tl)
+    (b_height) (b_height) board_background_color;
+  (*outline board*)
+  (*top line*)
+  draw_line (fst board_tl) (snd board_tl) (fst board_tr) (snd board_tr)
+    board_line_color;
+  (*bottom line*)
+  draw_line (fst board_bl) (snd board_bl) (fst board_br) (snd board_br)
+    board_line_color;
+  (*left line*)
+  draw_line (fst board_tl) (snd board_tl) (fst board_bl) (snd board_bl)
+    board_line_color;
+  (*right line*)
+  draw_line (fst board_tr) (snd board_tr) (fst board_br) (snd board_br)
+    board_line_color;
+  end_drawing ();
+  if is_key_down Key.B == false then draw_board ()
 
 (* initial window, all windows must be recursive*)
 let rec loop () =
@@ -37,10 +97,12 @@ let rec loop () =
     clear_background Color.white;
     (* custom function, prints message on the screen *)
     center_text "Welcome to Monopoly! Press the Space bar to continue...";
-    (*draw_text "Welcome to Monopoly! Press the Space bar to continue" (width / 4)
-      (height / 2) (height / 15) Color.green;*)
-    (* raylib function [is_key_down Key.key] is a bool tells us whether [key] is pressed or not*)
-    if is_key_down Key.Space then check_start ();
+    (*draw_text "Welcome to Monopoly! Press the Space bar to continue" (width /
+      4) (height / 2) (height / 15) Color.green;*)
+    (* raylib function [is_key_down Key.key] is a bool tells us whether [key] is
+       pressed or not*)
+    (*if is_key_down Key.Space then check_start ();*)
+    if is_key_down Key.Space then draw_board ();
     (* call at the end of each drawing *)
     end_drawing ();
     loop ()
--- src/gui.mli
+++ src/gui.mli
@@ -3,4 +3,5 @@ val height :int
 
 
 val setup : unit -> unit
+val draw_board : unit -> unit
 val loop : unit -> unit
