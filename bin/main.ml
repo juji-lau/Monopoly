f8831d9166ed081539e2fb0579771cbd565a795c
Sophie Zhao <sz266@cornell.edu>
2023-04-16 19:08:06 -0400

    Implemented json

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,5 @@
 open Monopoly
-open Raylib
+(* open Raylib *)
 
 open Player
 (** [play_game f] starts the adventure in file [f]. *)
@@ -46,7 +46,7 @@ let rec player flow play name =
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
-  Gui.setup () |> Gui.loop;
+  (* Gui.setup () |> Gui.loop; *)
   ANSITerminal.print_string [ ANSITerminal.red ] "\n\nWelcome to MONOPOLY! \n";
   print_endline "Please enter your name: \n";
   print_string "> ";
--- src/gui.mli
+++
@@ -1,7 +0,0 @@
-val width : int
-val height :int 
-
-
-val setup : unit -> unit
-val draw_board : unit -> unit
-val loop : unit -> unit
